78a6bd0b0bd2afd0932d05ececa16be418036a39
James Miller <jam1211miller@gmail.com>
2023-03-08 21:00:53 -0700

    Updated  code and readme

--- DeadlockAvoidance.c
+++ DeadlockAvoidance.c
@@ -23,41 +23,40 @@ void *process(void *arg)
 {
     int id = *(int *)arg;
     bool running = true;
-    unsigned int seed = time(NULL) + id; // initialize random seed for this thread
+    unsigned int seed = time(NULL) + id; // Initialize random seed for this thread.
 
     while (running)
     {
-        // try to acquire resource
+        // Try to acquire resource.
         pthread_mutex_lock(&mutex);
         if (resource == 0)
         {
             resource = id;
             fprintf(activityFile, "Process %d acquired resource\n", id);
-            printf("ProcessID: %d | ResourceID: %d\n", id, id);
+            printf("ProcessID: %d | ResourceID: %d | Value: %d \n", id, id, seed);
         }
         else
         {
             fprintf(activityFile, "Process %d is waiting for resource\n", id);
-            printf("ProcessID: %d | ResourceID: %d\n", id, id);
+            printf("ProcessID: %d | ResourceID: %d | Waiting\n", id, id);
             pthread_mutex_unlock(&mutex);
             sleep(rand_r(&seed) % RESOURCE_MAX_WAIT_TIME + 1);
             continue;
         }
         pthread_mutex_unlock(&mutex);
 
-        // use resource
+        // Use resource
         sleep(rand_r(&seed) % 3 + 1);
 
-        // release resource
+        // Release the resource.
         pthread_mutex_lock(&mutex);
         resource = 0;
 
         fprintf(activityFile, "Process %d released resource\n", id);
-        printf("ProcessID: %d | ResourceID: %d\n", id, id);
+        printf("ProcessID: %d | ResourceID: %d | Value: 0 \n", id, id);
         pthread_mutex_unlock(&mutex);
 
-        // check exit condition
-        // set running to false to exit the loop
+        // Set exit condition.
         running = false;
     }
 
@@ -68,7 +67,7 @@ int main()
 {
     printf("Opening activity log. Avoiding deadlock process. \n");
 
-    // initialize random number generator
+    // Initialize the random number generator.
     srand(time(NULL));
 
     activityFile = fopen("/Users/jamesmiller/Desktop/School/Operating Systems/DeadlockAvoidance/activityLog.txt", "a");
@@ -78,17 +77,17 @@ int main()
         return 1;
     }
 
-    // create processes
+    // Create processes.
     pthread_t threads[NUM_PROCESSES];
     int ids[NUM_PROCESSES];
 
     for (int i = 0; i < NUM_PROCESSES; i++)
     {
-        ids[i] = i + 1; // initialize the ids array
+        ids[i] = i + 1; // Initialize the ids array.
         pthread_create(&threads[i], NULL, process, &ids[i]);
     }
 
-    // wait for processes to finish
+    // Wait for processes to finish.
     for (int i = 0; i < NUM_PROCESSES; i++)
     {
         pthread_join(threads[i], NULL);
